8d358b5e477ccdf0c5b299175947b91bafc5a392
eiiizong <eiiizong@163.com>
2021-11-19 17:03:39 +0800

    路由

---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+/*
+ * @Author: zengxm
+ * @Date: 2021-11-19 17:01:22
+ * @LastEditors: zengxm
+ * @LastEditTime: 2021-11-19 17:01:23
+ * @Description: change
+ * @FilePath: /demo-vue3/src/router/index.ts
+ */
+import {
+    createRouter,
+    createWebHashHistory,
+    RouteRecordRaw
+  } from 'vue-router'
+  import Home from '@/views/home.vue'
+  import Vuex from '@/views/vuex.vue'
+  
+  const routes: Array<RouteRecordRaw> = [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/vuex',
+      name: 'Vuex',
+      component: Vuex
+    },
+    {
+      path: '/axios',
+      name: 'Axios',
+      component: () => import('@/views/axios.vue') // 懒加载组件
+    }
+  ]
+  
+  const router = createRouter({
+    history: createWebHashHistory(),
+    routes
+  })
+  
+  export default router
+  
