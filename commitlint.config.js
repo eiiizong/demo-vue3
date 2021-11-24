a7b5933ec9e8bacce2ce3c44ea12094f0044546e
eiiizong <eiiizong@163.com>
2021-11-24 15:08:21 +0800

    clear gh-pages

--- commitlint.config.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: zengxm
- * @Date: 2021-11-23 16:58:46
- * @LastEditors: zengxm
- * @LastEditTime: 2021-11-23 16:58:47
- * @Description: change
- * @FilePath: /demo-vue3/commitlint.config.js
- */
-module.exports = { extends: ['@commitlint/config-conventional'] }
