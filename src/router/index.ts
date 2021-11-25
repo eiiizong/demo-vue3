655e61edd7b9942f5f244f5fd1c63a6cf20e6ce9
eiiizong <eiiizong@163.com>
2021-11-25 14:27:50 +0800

    feat(components): test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,7 @@
-/*
- * @Author: zengxm
- * @Date: 2021-11-19 17:01:22
- * @LastEditors: zengxm
- * @LastEditTime: 2021-11-24 15:41:14
- * @Description: change
- * @FilePath: /demo-vue3/src/router/index.ts
- */
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
-import Home from '@/views/Home.vue'
+// import Home from '@/views/Home.vue'
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+const routes: Array<RouteRecordRaw> = []
 
 const router = createRouter({
   history: createWebHashHistory(),
